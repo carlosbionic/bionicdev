70c700b2cd9a593c8fc7242d5d682dd16dae562d
bionic <carlosaprado@gmail.com>
2022-03-28 14:16:05 -0300

    form

---
+++ action_page.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <title>Carlos | Physics, Python and more</title>
+  <link rel="shortcut icon" href="images/favicon.ico">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">  
+
+</head>
+<body>
+
+  <div class="header">
+    <div class="container">
+
+      <div id="header-repeat"></div>
+
+      <div class="row">
+        <div class="col-2">
+          <h1>Muebles de diseño<br>Hierro, madera, y más!</h1>
+          <p>Acá va un texto adicional, hablando<br>un poco sobre lo que ofrece la empresa.</p>
+          <a href="" class="btn">Explorar Ahora &#8594;</a>
+        </div>
+        <div class="col-2">
+          <!--<img src="images/image1.png">-->
+          <img src="images-ni/productos/sillas/2200.png">
+        </div>
+      </div>
+    </div> <!-- Cierra container -->
+  </div> <!-- Cierra header -->
+
+
+  <!------- categorias destacadas ------->
+  <!--
+  <div class="categories">
+    <div class="small-container">
+      <div class="row">
+        <div class="col-3">
+          <img src="images-ni/productos/mesas/mesavidrio1.jpg">
+        </div>
+        <div class="col-3">
+          <img src="images-ni/productos/sillones/sillon1up.jpg">
+        </div>
+        <div class="col-3">
+          <img src="images-ni/productos/rack/rack1up.jpg">
+        </div>
+      </div>
+    </div>
+  </div>
+  -->
+<br>
+  <!------- productos destacados ------->
+  <div class="small-container">
+    <h2 class="title">Productos Destacados</h2>
+    <div class="row">
+      <div class="col-4">
+        <img src="images/product-1.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images/product-2.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images/product-3.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images/product-4.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+    </div>
+
+<!---------- Categorias ------------->
+    <h2 class="title">Categorías</h2>
+    <div class="row">
+      <div class="col-4">
+        <img src="images-ni/categoria-rack.jpg">    
+        <h4>Rack</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images-ni/categoria-sillas.jpg">
+        <h4>Sillas</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images-ni/categoria-sillones.jpg">
+        <h4>Sillones</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images-ni/categoria-mesas.jpg">
+        <h4>Mesas</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+    </div> <!-- Cierra row -->
+    <div class="row">
+      <div class="col-4">
+        <img src="images/product-9.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images/product-10.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images/product-11.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-half-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+      <div class="col-4">
+        <img src="images/product-12.jpg">
+        <h4>Red Printed T-shirt</h4>
+        <!--
+        <div class="rating">
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star"></i>
+          <i class="fa fa-star-o"></i>
+        </div>
+        <p>$50.00</p>
+        -->
+      </div>
+    </div> <!-- Cierra row -->
+  </div> <!-- Cierra small-container -->
+
+<!-------- Ofertas --------->
+  <div class="offer">
+
+    <div class="small-container">
+      <div class="row">
+
+        <div class="col-2">
+          <img src="images-ni/nuevas/11211549_10205309888720255_986095692_o.jpg" class="offer-img">
+        </div>
+        <div class="col-2">
+          <p>Contenido Exclusivo en New Iron</p>
+          <h1>Sábanas</h1>
+          <small>Las sábanas que ofrecemos vienen en distintos tamaños, colores y texturas. Diferentes marcas trabajan con nosotros desde hace 10 años. Tenemos una gran variedad de este tipo de productos en nuestra tienda.</small>
+          <a href="" class="btn">Comprar Ahora &#8594;</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!-------- Ofertas --------->
+  <div class="offer">
+    <h2 class="title">Contactanos</h2>
+    <div class="small-container">
+      <div class="row">
+        <div class="col-2">
+
+          <?php
+          $ip = $_SERVER['REMOTE_ADDR'];
+          $nomb = $_GET["name"];
+          $mail = $_GET["mail"];
+          $phone = $_GET["phone"];
+          $comment = $_GET["comment"];
+
+          $to = "prado@new-iron.com.ar,carlosaprado@gmail.com";
+          $subject = "New Iron";
+
+          $msg = "
+                   ip: $ip \n
+                   Nombre: $nomb \n
+                   e-mail: $mail \n
+                   Telefono: $phone \n
+                   Comentario: $comment \n"; 
+
+          $header = "From: prado@new-iron.com.ar\r\n"; 
+          $header.= "MIME-Version: 1.0\r\n"; 
+          $header.= "Content-Type: text/plain; charset=ISO-8859-1\r\n"; 
+          $header.= "X-Priority: 1\r\n"; 
+
+          $status = mail($to,$subject,$msg,$header);
+
+          if($status)
+          { 
+              echo '<p>Su correo ha sido enviado correctamente!</p>';
+          } else { 
+              echo '<p>Algo salio mal, por favor intente nuevamente.</p>'; 
+          }
+          ?>
+
+
+          <p style="width: 40%; align: justify;"><?php echo $nomb; ?>, gracias por su consulta!</p>
+          <p style="width: 40%; align: justify;">Su e-mail es: <?php echo $mail; ?></p>
+          <p style="width: 40%; align: justify;">Su teléfono: <?php echo $phone; ?></p><br>
+          <p style="width: 40%; align: justify;">Su comentario: <?php echo $comment; ?></p><br><br>
+
+          <p>En breve nos comunicaremos con usted. Que tenga un buen día!</p>
+
+        </div> <!-- Cierra col-2 -->
+
+        <div class="col-2" style="border: solid black;">
+
+          <h1 style="text-align: center;">Visitanos</h1>
+
+          <div id="map"></div>
+
+          <script>
+          function myMap() {
+          var myCenter = new google.maps.LatLng(-34.645860, -58.593151);
+          var mapOptions = {
+              center: myCenter,
+              zoom: 10
+          }
+          var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+          var marker = new google.maps.Marker({position:myCenter});
+          marker.setMap(map);
+          }
+          </script>
+
+          <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAoJYhRSKEGq-zZimxGQWXEcWR4Ux8Xxxo&callback=myMap"></script>
+          
+        </div> <!-- Cierra col-2 -->
+      </div> <!-- Cierra row -->
+    </div> <!-- Cierra small-container -->
+  </div> <!-- Cierra offer -->
+
+
+<!--------- marcas ----------->
+  <div class="brands">
+    <div class="small-container">
+      <div class="row">
+        <div class="col-5">
+          <img src="images-ni/logos/logo-nautica.png">
+        </div>
+        <div class="col-5">
+          <img src="images-ni/logos/logo-danubio.jpg">
+        </div>
+        <div class="col-5">
+          <img src="images-ni/logos/logo-simmons.jpg">
+        </div>
+        <div class="col-5">
+          <img src="images-ni/logos/logo-belmo.png">
+        </div>
+        <div class="col-5">
+          <img src="images-ni/logos/logo-suplesa.png">
+        </div>
+      </div>
+    </div>
+  </div>
+
+<!--------- footer ----------->
+
+<div id="footer-repeat"></div>
+
+<!---------- js for toggle menu --------->
+  <script>
+    var MenuItems = document.getElementById("MenuItems");
+
+    MenuItems.style.maxHeight = "0px";
+
+    function menutoggle(){
+      if(MenuItems.style.maxHeight == "0px")
+        {
+          MenuItems.style.maxHeight = "200px";
+        }
+      else
+        {
+          MenuItems.style.maxHeight = "0px";
+        }
+    }
+  </script>
+
+</body>
+</html>
